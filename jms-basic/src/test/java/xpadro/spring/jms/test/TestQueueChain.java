645f2f1e31bb207f2a35eb29888463ac8e46200b
larsbohl <larsbohl@gmail.com>
2015-11-14 20:15:50 +0000

    chaining queues

--- jms-basic/src/test/java/xpadro/spring/jms/test/TestQueueChain.java
+++ jms-basic/src/test/java/xpadro/spring/jms/test/TestQueueChain.java
@@ -12,6 +12,7 @@ import xpadro.spring.jms.receiver.NotificationReceiver;
 
 import javax.jms.*;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -27,45 +28,65 @@ public class TestQueueChain {
   public static final int TOTAL_MESSAGES = 100;
 
   @Autowired
-	@Qualifier("jmsTemplate")
+  @Qualifier("jmsTemplate")
   private JmsTemplate jmsTemplate;
 
   @Autowired
   private ConnectionFactory connectionFactory;
 
-
   @Autowired
   @Qualifier("asyncTestQueue")
   private Queue queue;
 
+  @Autowired
+  @Qualifier("asyncTestQueue2")
+  private Queue queue2;
+
   private Stream<Notification> sourceStream() {
     ThreadLocalRandom random = ThreadLocalRandom.current();
     return IntStream.range(0, TOTAL_MESSAGES).boxed().map(i ->
-      new Notification("" + i, "M" + random.nextInt())
+        new Notification("" + i, "M" + random.nextInt())
     );
   }
 
   @Test
   public void testTopicSending() throws Exception {
-
     Connection con = connectionFactory.createConnection();
     con.start();
     ExecutorService executor = Executors.newCachedThreadPool();
-    NotificationReceiver receiver = createDynamicReceiver(con);
+    NotificationReceiver receiver = createDynamicReceiver(con, queue);
+    NotificationReceiver receiver2 = createDynamicReceiver(con, queue2);
     Callable<Void> producer = () -> {
-        sourceStream().forEach(notification -> jmsTemplate.convertAndSend(queue, notification));
-        return null;
+      sourceStream().forEach(notification -> jmsTemplate.convertAndSend(queue, notification));
+      return null;
     };
-    Callable<Long> consumer = () -> receiver.openStream().count();
+    Callable<Long> consumer = () -> {
+      long[] count = new long[1];
+      receiver.openStream().forEach(notification -> {
+        jmsTemplate.convertAndSend(queue2, notification);
+        count[0]++;
+      });
+      return count[0];
+    };
+    Callable<Long> consumer2 = () -> {
+      long[] count = new long[1];
+      receiver2.openStream().forEach(notification -> {
+        if (notification != null) {
+          count[0]++;
+        }
+      });
+      return count[0];
+    };
     executor.submit(producer);
-    Future<Long> consumerFuture = executor.submit(consumer);
+    executor.submit(consumer);
+    Future<Long> consumerFuture = executor.submit(consumer2);
     assertThat((long) TOTAL_MESSAGES, is(consumerFuture.get(1, TimeUnit.MINUTES)));
     executor.shutdown();
     con.close();
 
   }
 
-  private NotificationReceiver createDynamicReceiver(Connection con) throws JMSException {
+  private NotificationReceiver createDynamicReceiver(Connection con, Queue queue) throws JMSException {
     Session session = con.createSession(false, Session.AUTO_ACKNOWLEDGE);
     MessageConsumer consumer = session.createConsumer(queue);
     NotificationReceiver listener = new NotificationReceiver(TOTAL_MESSAGES);
